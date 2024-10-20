8c7e5f3a6653dc12bae59f786f78bf8ee866445b
hfsdo <thijsdl@gmail.com>
2024-10-20 14:39:22 +0200

    finished UI

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -2,29 +2,49 @@ import tkinter as tk
 
 window = tk.Tk()
 
-Frame1 = tk.Frame(master=window, height=10)
-Frame1.pack(fill=tk.X, expand=True)
+#Frame1 = tk.Frame(master=window, height=10)
+#Frame1.pack(fill=tk.X, expand=True)
 
-Frame2 = tk.Frame(master=window)
-Frame2.pack()
+#Frame2 = tk.Frame(master=window)
+#Frame2.pack()
 
-label1 = tk.Label(master=Frame1, text="010")
-label1.pack(fill=tk.Y, side=tk.LEFT, expand=False)
+frame1 = tk.Frame(
+    master=window,
+    relief=tk.RAISED,
+    borderwidth=1
+)
+frame1.grid(row=0,column=0)
+label1 = tk.Label(master=frame1, text="010")
+label1.pack()
 
-button = tk.Button(master=Frame1, text="reset")
-button.pack(fill=tk.Y, side=tk.TOP, expand=False)
+frame2 = tk.Frame(
+    master=window,
+    relief=tk.RAISED,
+    borderwidth=1
+)
+frame2.grid(row=0,column=4,columnspan=2)
+button = tk.Button(master=frame2, text="reset")
+button.pack()
 
-label2 = tk.Label(master=Frame1, text="000")
-label2.pack(fill=tk.Y, side=tk.RIGHT, expand=False)
+frame3 = tk.Frame(
+    master=window,
+    relief=tk.RAISED,
+    borderwidth=1
+)
+frame3.grid(row=0,column=9)
+label2 = tk.Label(master=frame3, text="000")
+label2.pack()
+
+
 
 for i in range(10):
     for j in range(10):
         frame = tk.Frame(
-            master=Frame2,
+            master=window,
             relief=tk.RAISED,
             borderwidth=1
         )
-        frame.grid(row=i, column=j)
+        frame.grid(row=i+1, column=j)
         #label = tk.Label(master=frame, text=f"row {i}\nColumn {j}")
         #label.pack()
         button = tk.Button(
