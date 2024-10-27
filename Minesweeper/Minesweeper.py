9dfd4c24440caaf8d1af900739223bf7f62aab58
hfsdo <thijsdl@gmail.com>
2024-10-27 15:04:30 +0100

    added detection of mines

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -14,15 +14,34 @@ def resetGame():
     button16.pack()
     
 def click(frame, button):
+    global grid
+    global lost
     info = frame.grid_info()
     x=info["row"]-1
     y=info["column"]
     print(f"({x}, {y})")
+    if lost:
+        return
     button.config(relief=tk.SUNKEN, text=grid[x][y])
+    count = 0
+    if grid[x][y] == "":
+        for i in range(x-1,x+2):
+            for j in range(y-1,y+2):
+                if i >= 0 and i < len(grid):
+                    if j >= 0 and j < len(grid[i]):
+                        if grid[i][j] == "*":
+                            count += 1
+        if count != 0:
+            button.config(text=count)
+    elif grid[x][y] == "*":
+        button.config(bg='red')
+        lost = True
     
 def createGrid(x, y, mines):
     global grid
+    global lost
     grid = []
+    lost = False
     for i in range(x):
         row = []
         for j in range(y):
