c07cd8b21ee9c657b8c68b3078b9d66738ae0b95
hfsdo <thijsdl@gmail.com>
2024-11-02 13:54:42 +0100

    added right click flagging

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -13,35 +13,58 @@ def resetGame():
     button16 = tk.Button(master=window, text="Hard", command= lambda: createBoard(16, 30, 99))
     button16.pack()
     
-def click(frame, button):
+def click(frame, button, leftclick):
     global grid
     global lost
+    global minecount
+    global labelMines
     info = frame.grid_info()
     x=info["row"]-1
     y=info["column"]
     print(f"({x}, {y})")
     if lost:
         return
-    button.config(relief=tk.SUNKEN, text=grid[x][y])
-    count = 0
-    if grid[x][y] == "":
-        for i in range(x-1,x+2):
-            for j in range(y-1,y+2):
-                if i >= 0 and i < len(grid):
-                    if j >= 0 and j < len(grid[i]):
-                        if grid[i][j] == "*":
-                            count += 1
-        if count != 0:
-            button.config(text=count)
-    elif grid[x][y] == "*":
-        button.config(bg='red')
-        lost = True
+    if not leftclick:
+        if button["text"] == "":
+            minecount -= 1
+            button.config(text="F")
+            labelMines.config(text=f"{minecount:03d}")
+        else:
+            minecount += 1
+            button.config(text="")
+            labelMines.config(text=f"{minecount:03d}")
+    elif button["text"] != "F":
+        button.config(relief=tk.SUNKEN, text=grid[x][y])
+        count = 0
+        if grid[x][y] == "":
+            for i in range(x-1,x+2):
+                for j in range(y-1,y+2):
+                    if i >= 0 and i < len(grid):
+                        if j >= 0 and j < len(grid[i]):
+                            if grid[i][j] == "*":
+                                count += 1
+            if count != 0:
+                button.config(text=count)
+        elif grid[x][y] == "*":
+            button.config(bg='red')
+            lost = True
+
+
+
+def rightClick(event):
+    print("right")
+    button = event.widget
+    frame = button.master
+    click(frame, button, False)
+    
     
 def createGrid(x, y, mines):
     global grid
     global lost
+    global minecount
     grid = []
     lost = False
+    minecount = mines
     for i in range(x):
         row = []
         for j in range(y):
@@ -61,6 +84,8 @@ def createGrid(x, y, mines):
 
 def createBoard(x, y, mines):
     createGrid(x,y,mines)
+    global labelMines
+    
     for widget in window.winfo_children():
         widget.destroy()
 
@@ -70,8 +95,8 @@ def createBoard(x, y, mines):
         borderwidth=1
     )
     frame1.grid(row=0,column=0)
-    label1 = tk.Label(master=frame1, text="010")
-    label1.pack()
+    labelMines = tk.Label(master=frame1, text="010")
+    labelMines.pack()
 
     frame2 = tk.Frame(
         master=window,
@@ -104,7 +129,8 @@ def createBoard(x, y, mines):
                 height=1
                 #text = f"({i},{j})"
             )
-            button.config(command=lambda frame=frame, button=button: click(frame,button))
+            button.config(command=lambda frame=frame, button=button: click(frame,button,True))
+            button.bind("<Button-3>", rightClick)
             button.pack()
 
 window = tk.Tk()
